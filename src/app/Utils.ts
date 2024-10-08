d8e83d9ea511cf89b37cff08085a9b937e67580d
devendra kumar rathor <devendra143143@gmail.com>
2024-10-08 16:21:12 +0530

    add some error handling featrue

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,30 @@
-export function toUpperCase(arg:string){
-    return arg.toUpperCase();
+export type StringCaseType = {
+  lowerCase: string;
+  upperCase: string;
+  length: number;
+  characters: Array<string>;
+  extra: object;
+};
+
+export function checkAllStringCaseAndChar(arg: string): StringCaseType {
+  return {
+    lowerCase: arg.toLowerCase(),
+    upperCase: arg.toUpperCase(),
+    length: arg.length,
+    characters: Array.from(arg),
+    extra: {},
+  };
 }
+
+export function toUpperCase(arg: string) {
+  return arg.toUpperCase();
+}
+
+export class StringUtils{
+    public toUpperCase(arg:string){
+        if(!arg){
+            throw new Error('Invalid argument!')
+        }
+        return arg.toUpperCase();
+    }
+  }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,9 @@
-import { toUpperCase } from "../app/Utils";
+import {
+  checkAllStringCaseAndChar,
+  StringCaseType,
+  StringUtils,
+  toUpperCase,
+} from "../app/Utils";
 
 describe("Utils it suite", () => {
   it("Should return uppercase", () => {
@@ -44,5 +49,61 @@ describe("Utils it suite", () => {
     const actual = sute("@#$@#");
 
     expect(actual).toBe(expected);
+  });
+  it("checking checkAllStringCaseAndChar ", () => {});
+});
+describe("checking checkAllStringCaseAndChar", () => {
+  const expected: StringCaseType = {
+    lowerCase: "abc",
+    upperCase: "ABC",
+    length: 3,
+    characters: ["a", "B", "c"],
+    extra: {},
+  };
+  const sute = checkAllStringCaseAndChar;
+  const actual = sute("aBc");
+
+  it("string should be lowercase", () => {
+    expect(actual.lowerCase).toBe(expected.lowerCase);
+  });
+  it("string should be uppercase", () => {
+    expect(actual.upperCase).toBe(expected.upperCase);
+  });
+  it("checking length", () => {
+    expect(actual.length).toBe(expected.length);
+  });
+  it("checking characters", () => {
+    expect(actual.characters).toEqual(expected.characters);
+  });
+  it("extra shuld be object", () => {
+    expect(actual.extra).toEqual(expected.extra);
+  });
+});
+
+describe("checking string shuld be lowercase", () => {
+  it.each([
+    { input: "aBc", expected: "abc" },
+    { input: "aBC", expected: "abc" },
+    { input: "abc", expected: "abc" },
+    { input: "MY-STRING", expected: "my-string" },
+  ])("$input toLowerCase is $expected", ({ input, expected }) => {
+    const actual = checkAllStringCaseAndChar(input);
+    expect(actual.lowerCase).toBe(expected);
+  });
+});
+
+describe("StringUtils suite", () => {
+  let suite: StringUtils;
+
+  beforeEach(() => {
+    suite = new StringUtils();
+  });
+
+  it("Should throw error on Invalid argument", () => {
+    function expectedError() {
+      suite.toUpperCase("");
+    }
+    expect(expectedError).toThrow();
+    expect(expectedError).toThrow('Invalid argument!')
   });
 });
