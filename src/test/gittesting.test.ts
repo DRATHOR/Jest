56034668dce0b72c60113cb58f3788fa8a61f70a
devendra kumar rathor <devendra143143@gmail.com>
2024-10-09 15:32:16 +0530

    checking revert commit senario

---
+++ src/test/gittesting.test.ts
@@ -0,0 +1,8 @@
+describe(
+    "just testing the revert command",()=>{
+        it('I am reverting the latest commit ',()=>{
+            const actual='abc';
+            expect(actual).toBe('abc');
+        })
+    }
+);
